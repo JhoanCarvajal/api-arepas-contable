5ae42d965ee8391e114034a7165aac2e6ee3b126
Jhoan Carvajal <jhoans127@outlook.es>
2025-09-28 04:50:33 -0500

    fix: change weeklyBalance to null

--- expenses/models.py
+++ expenses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Expenses(models.Model):
     id = models.BigIntegerField(primary_key=True)
-    weeklyBalance = models.ForeignKey('weeklybalances.WeeklyBalance', on_delete=models.CASCADE, related_name='expenses')
+    weeklyBalance = models.ForeignKey('weeklybalances.WeeklyBalance', on_delete=models.SET_NULL, related_name='expenses', null=True, blank=True)
     date = models.DateField()
     earnings = models.IntegerField()
     createdAt = models.DateTimeField(auto_now_add=True)
